db7fae8e8c43b9c32ed473ede176d4e54157c4c0
Swapna V <vswapna3202@gmail.com>
2023-09-20 22:04:51 +0100

    feat: refactored code

--- src/main/java/com/techreturners/app/TerminateBooking.java
+++ src/main/java/com/techreturners/app/TerminateBooking.java
@@ -8,6 +8,7 @@ import java.util.ArrayList;
 
 public class TerminateBooking extends Thread {
     private final BookingApp bookingApp;
+    private static final int TOTAL_SEATS = 15;
     public TerminateBooking(BookingApp bookingApp){
         this.bookingApp = bookingApp;
     }
@@ -17,9 +18,12 @@ public class TerminateBooking extends Thread {
 
         while(true){
             try {
+                //Check availableSeats on booking object if all seats have been booked
+                // If true invoke terminateBooking method on bookingApp which terminates
+                // the app
                 Booking booking = new Booking();
                 ArrayList<SeatNumber> seatNumbers = bookingApp.checkAvailableSeats(booking);
-                if (seatNumbers.size() >= 15){
+                if (seatNumbers.size() >= TOTAL_SEATS){
                     bookingApp.terminateBooking();
                     break;
                 }
@@ -27,6 +31,8 @@ public class TerminateBooking extends Thread {
                 System.out.println("Error in reading file seatMapping.txt"
                         +ioe.getMessage());
             }
+            // Thread sleeps for 10 s and checks again if all seats have been filled
+            // and app needs to terminate
             try{
                 Thread.sleep(10000);
             }catch(InterruptedException ie){
